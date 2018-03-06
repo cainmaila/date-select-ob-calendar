6cf67af1ba6551a8c931557545292198dabc4d5f
CainChu <cainmaila@gmail.com>
2018-03-06 16:07:27 +0800

    DEMO

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # date-select-ob-calendar
-use date-select-ob calendar demo
+
+這是一個日歷範本，他使用了 [DeteSelectOb](https://www.npmjs.com/package/date-select-ob) 作為資料基底
+
+## Build Setup
+
+```bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+## run start
+
+```bash
+npm run start
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="hello">
+        <DateSelect @select="selectDate"></DateSelect>
+    </div>
+</template>
+
+<script>
+import DateSelect from './DateSelect'
+export default {
+    name: 'HelloWorld',
+    data() {
+        return {}
+    },
+    components: {
+        DateSelect
+    },
+    methods: {
+        selectDate(date) {
+            console.log(JSON.stringify(date))
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
